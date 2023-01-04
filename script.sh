c511b42c9e17d7155dfbe3594bc551bad150b091
Sebastian Marulanda <smarulanda@gmail.com>
2023-01-03 23:27:46 -0500

    Convert action from Docker-based to composite-based (#1)
    * convert to composite action
    
    * Update script.sh

--- script.sh
+++ script.sh
@@ -31,10 +31,22 @@ function api_request {
     local endpoint=$2
     local data=${3:-"{}"}
 
-    curl -s -X "$method" "https://forge.laravel.com/api/v1/servers/$server_id/sites/$site_id/$endpoint" \
-        -H "Authorization: Bearer $api_token" \
-        -H "Content-Type: application/json" \
-        -d "$data"
+    code=$(curl \
+        --request "$method" \
+        --header "Authorization: Bearer $api_token" \
+        --header "Accept: application/json" \
+        --header "Content-Type: application/json" \
+        --data "$data" \
+        --output /dev/null \
+        --write-out "%{http_code}" \
+        --silent \
+        "https://forge.laravel.com/api/v1/servers/$server_id/sites/$site_id/$endpoint")
+    
+    # Check for an HTTP response code of 200
+    if [[ "$code" -ne 200 ]]; then
+        echo "Forge API error encountered."
+        exit 1
+    fi
 }
 
 # Update the .env file
@@ -45,6 +57,7 @@ if [[ -n "$env_file_path" ]]; then
     # Create a JSON object with "content" property
     data=$(jq -n --arg content "$file_contents" '{content: $content}')
 
+    echo "Updating the .env file..."
     api_request "PUT" "env" "$data"
 fi
 
@@ -56,8 +69,10 @@ if [[ -n "$deploy_script_path" ]]; then
     # Create a JSON object with "content" property
     data=$(jq -n --arg content "$file_contents" '{content: $content}')
 
+    echo "Updating the deploy script..."
     api_request "PUT" "deployment/script" "$data"
 fi
 
 # Trigger the deployment
+echo "Deploying..."
 api_request "POST" "deployment/deploy"
